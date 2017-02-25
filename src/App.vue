61ab942fbc35e64702b45514f846c827b97a79c2
Drake <dascott0204@gmail.com>
2017-02-24 20:07:17 -0600

    Finished setting up the dynamic component but I am having trouble figuring out how to pass my content into the slot

--- src/App.vue
+++ src/App.vue
@@ -3,13 +3,13 @@
         <div class="row">
             <div class="col-xs-12">
                 <br>
-                <button class="btn btn-primary">Load Blue Template</button>
-                <button class="btn btn-success">Load Green Template</button>
-                <button class="btn btn-danger">Load Red Template</button>
+                <button class="btn btn-primary" @click="selectedColor = 'appBlue'">Load Blue Template</button>
+                <button class="btn btn-success" @click="selectedColor = 'appGreen'">Load Green Template</button>
+                <button class="btn btn-danger" @click="selectedColor = 'appRed'">Load Red Template</button>
                 <hr>
-                <app-blue></app-blue>
-                <app-green></app-green>
-                <app-red></app-red>
+                <component :is="selectedColor">
+                    <p slot="content">{{ greeting }}</p>
+                </component>
             </div>
         </div>
     </div>
@@ -21,6 +21,12 @@
     import Red from './components/Red.vue';
 
     export default {
+        data: function () {
+            return {
+                selectedColor: 'appRed',
+                greeting: 'Hello User! How can I help?'
+            };
+        },
         components: {
             appBlue: Blue,
             appGreen: Green,
